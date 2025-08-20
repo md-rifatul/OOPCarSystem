36f7ca84f1acbcdc84c5744a44eda211c02764da
Rifatul <rifatul.ism@gmail.com>
2025-08-21 01:48:21 +0600

    Implement Car class with properties, Drive method, and ShowCarInfo

---
+++ OOPCarSystem/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCarSystem
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+
+        public Car(string brand, string model, int year, int mileage)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+
+        public void Drive(int kilometers)
+        {
+            Mileage += kilometers;
+            Console.WriteLine($"Car {Brand} {Model} has driven {kilometers} km. Total mileage: {Mileage} km.");
+        }
+    }
+}
---
+++ OOPCarSystem/Program.cs
@@ -0,0 +1,8 @@
+﻿using OOPCarSystem;
+
+Car car1 = new Car("Toyota","Corolla",2010,0);
+Car car2 = new Car("Tesla","Model 3",2023,0);
+
+car1.Drive(50);
+car2.Drive(120);
+car1.Drive(30);
